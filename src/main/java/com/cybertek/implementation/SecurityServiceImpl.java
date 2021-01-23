c0820df5019b67129c850c854341d254ccd3ff5b
Talha Turkyolu <turkyolutalha@gmail.com>
2021-01-22 20:44:03 -0600

    first commit

---
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -0,0 +1,31 @@
+package com.cybertek.implementation;
+
+import com.cybertek.entity.User;
+import com.cybertek.entity.common.UserPrincipal;
+import com.cybertek.repository.UserRepository;
+import com.cybertek.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(s);
+
+        if(user==null){
+            throw new UsernameNotFoundException("This user does not exists");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
